6cb8edba112cdba462975d1eaae031c710a080e9
Jeaninemtn <97783113+Jeaninemtn@users.noreply.github.com>
2022-06-17 00:00:28 +0800

    Update

--- info-test.js
+++ info-test.js
@@ -10,3 +10,5 @@ for(let i of items){
     console.log(i.querySelector('.LongInfoCard_Type_Address span').innerText);
 
 }
+
+// https://eyesofkids.gitbooks.io/javascript-start-from-es6/content/part4/rest_spread.html
